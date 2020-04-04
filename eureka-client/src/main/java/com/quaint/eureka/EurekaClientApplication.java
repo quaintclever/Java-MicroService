5e59f6e9347df5d1928c2cbc9087d06db505a9dd
quaint <qicong@ilarge.cn>
2020-04-04 22:34:43 +0800

    eureka 代码练习

---
+++ eureka-client/src/main/java/com/quaint/eureka/EurekaClientApplication.java
@@ -0,0 +1,20 @@
+package com.quaint.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author quaint
+ * 04 April 2020
+ * @since 1.26
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class,args);
+    }
+
+}
---
+++ eureka-server/src/main/java/com/quaint/eureka/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.quaint.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author quaint
+ * 04 April 2020
+ * @since 1.26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+
+}
